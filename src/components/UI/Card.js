c7b3e40f4eb3976671c976ca6e5965406ca779bd
indu kushwaha <goforindu@gmail.com>
2023-02-24 12:17:02 +0530

    onMouseenter event added

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -4,15 +4,15 @@ function Card(props){
         props.clickhandle(props.alphabet.audio_desc_url);
      }
     return(
-        <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
+        <div className="flip-card"  >
+        <div className="flip-card-inner" onMouseEnter={clickhandler}>
+          <div className="flip-card-front" >
             <h1>{props.alphabet.name}</h1>
             <p>{props.alphabet.desc}</p>
             <br/>
             <img src={props.alphabet.img_url} alt={props.alphabet.desc}   width="120" height="120" />
           </div>
-          <div className="flip-card-back" onMouseOver={clickhandler}>
+          <div className="flip-card-back">
             <img src={props.alphabet.img_url} alt={props.alphabet.desc} data-el={props.alphabet.audio_desc_url}  width="100%" height="100%" />
            
           </div>
