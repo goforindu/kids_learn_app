cec09c9d0feefc791f0d6be9ed7300d1aeff9291
indu kushwaha <goforindu@gmail.com>
2023-03-02 17:52:31 +0530

    base url for json removed alphabets

--- src/api/alphabets.js
+++ src/api/alphabets.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
    // baseURL:"http://localhost:3006"
-    baseURL:"https://my-json-server.typicode.com/goforindu/json-server-app/alphabets"
+    baseURL:"https://my-json-server.typicode.com/goforindu/json-server-app"
 });
