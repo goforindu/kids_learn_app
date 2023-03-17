0613088f9e840aac4576bb13323c96fc73186fbd
indu kushwaha <goforindu@gmail.com>
2023-03-17 16:35:58 +0530

    urls modified

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,22 +3,21 @@ import React from 'react';
 function Card(props){
  // const base_url="http://localhost:3000";
 const  base_url="https://kidsappdata.netlify.app";
-let shapes;
-let img_desc_url;
+const url_img=base_url+"/assets/"+props.type+"/"+props.alphabet.img_url;
+const url_audio=base_url+"/assets/"+props.type+"/audio/"+props.alphabet.audio_desc_url;
+let back_img;
 if(props.type==="shapes" && (props.alphabet.name==="Square" || props.alphabet.name==="Rectangle" )){
-  img_desc_url=base_url+props.alphabet.img_url;
-  shapes=<img src={img_desc_url} alt={props.alphabet.desc} data-el={base_url+props.alphabet.audio_desc_url}  width="100%"/>
+  back_img=<img src={url_img} alt={props.alphabet.desc} data-el={base_url+props.alphabet.audio_desc_url}  width="100%"/>
 }
 else{
-  img_desc_url=base_url+props.alphabet.img_url;
-  shapes=<img src={img_desc_url} alt={props.alphabet.desc} data-el={base_url+props.alphabet.audio_desc_url}  width="100%" height="100%"/>
+  back_img=<img src={url_img} alt={props.alphabet.desc} data-el={base_url+props.alphabet.audio_desc_url}  width="100%" height="100%"/>
 }
 
 
-  const img_url=base_url+props.alphabet.img_url;
+ 
      const clickhandler=()=>{
         console.log("clicked");
-        props.clickhandle(base_url+props.alphabet.audio_desc_url);
+        props.clickhandle(url_audio);
      }
     return(
         <div className="flip-card"  >
@@ -27,10 +26,10 @@ else{
             <h3>{props.alphabet.name}</h3>
             <p>{props.alphabet.desc}</p>
             <br/>
-            <img src={img_url} alt={props.alphabet.desc}   width="120" height="120" />
+            <img src={url_img} alt={props.alphabet.desc}   width="120" height="120" />
           </div>
           <div className="flip-card-back">
-           {shapes}
+           {back_img}
           </div>
         </div>
       </div>
