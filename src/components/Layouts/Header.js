0fa2bb4d2bcd9409b98b1f5083a728e900f87bb6
indu kushwaha <goforindu@gmail.com>
2023-03-21 14:01:42 +0530

    audio player added for background music

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './header.module.css';
 const Header=(props)=>{
    const heading=props.heading;
-   
+   //const base_url="http://localhost:3000";
+   const  base_url="https://kidsappdata.netlify.app";
+   const background_music_url=base_url+"/assets/media/backgroundmusic.mp3";
+   useEffect(()=>{
+      document.getElementById("backgroundSound").volume=0.1;
+   },[])
  return(
+  <div> 
+        <audio src={background_music_url} id="backgroundSound" autoPlay loop></audio>
+         
         <div className={styles.topnav}>{heading}</div>
+  </div>
     );
 
 }
